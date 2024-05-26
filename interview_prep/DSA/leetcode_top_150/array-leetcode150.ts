4380fa92cc684ea34fae03b867824f1972e445bb
MCSpraint <mcspraint954@gmail.com>
2024-05-26 20:53:10 +0530

    added jump game 2

--- interview_prep/DSA/leetcode_top_150/array-leetcode150.ts
+++ interview_prep/DSA/leetcode_top_150/array-leetcode150.ts
@@ -1,31 +1,15 @@
 export class ArrayLeetcode150 {
-  static removeElement(nums: number[], val: number): number {
-    let k = 0; // Index to keep track of the position for non-val elements
-
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
-      // If the current element is not equal to val, move it to the position indicated by k
-      if (nums[i] !== val) {
-        nums[k] = nums[i];
-        k++;
+  static majorityElementBoyerMoore(a: number[]): number {
+    let majorityElement = a[0],
+      count = 1;
+    for (let i = 1; i < a.length; i++) {
+      count += a[i] === majorityElement ? 1 : -1;
+      if (count === 0) {
+        count = 1;
+        majorityElement = a[i];
       }
     }
-
-    return k; // Return the number of non-val elements
-  }
-  static jump(nums: number[]): boolean {
-    let lastPosition = nums.length - 1; // Last index to reach
-
-    // Iterate backwards from the second last position
-    for (let i = nums.length - 2; i >= 0; i--) {
-      // Check if the maximum jump length from the current position can reach or go beyond lastPosition
-      if (nums[i] >= lastPosition - i) {
-        lastPosition = i; // Update lastPosition to the current index
-      }
-    }
-
-    // If lastPosition reaches the start (0 index), it means it's possible to reach the last index
-    return lastPosition === 0;
+    return majorityElement;
   }
   static candy(ratings: number[]) {
     const n = ratings.length;
@@ -36,7 +20,6 @@ export class ArrayLeetcode150 {
         candies[i] = candies[i - 1] + 1;
       }
     }
-
     for (let i = n - 2; i >= 0; i--) {
       if (ratings[i] > ratings[i + 1]) {
         candies[i] = Math.max(candies[i], candies[i + 1] + 1);
@@ -45,21 +28,37 @@ export class ArrayLeetcode150 {
 
     return candies.reduce((total, current) => total + current, 0);
   }
-
-  static hIndex(citations: number[]): number {
-    citations.sort((a, b) => a - b);
-
-    let hIndex = 0;
-    let n = citations.length;
-
-    for (let i = 0; i < n; i++) {
-      // The h-index is the maximum value of h such that citations[i] >= i + 1
-      if (citations[i] >= n - i) {
-        hIndex = Math.max(hIndex, n - i);
-      }
+  static jumpGame(nums: number[]): boolean {
+    let goal = nums.length - 1;
+    for (let i = nums.length - 2; i >= 0; i--)
+      if (i + nums[i] >= goal) goal = i;
+    return goal === 0;
+  }
+  static jumpGame2(nums: number[]): number {
+    if (nums.length <= 1) return 0;
+    let jumps = 0;
+    for (
+      let [l, r, farthest] = [0, 0, 0];
+      r < nums.length - 1;
+      [l, r] = [r + 1, farthest]
+    ) {
+      for (let i = l; i < r + 1; i++)
+        farthest = Math.max(i + nums[i], farthest);
+      jumps++;
     }
+    return jumps;
+  }
+  static maxProfit(prices: number[]): number {
+    if (prices.length === 0) return 0;
 
-    return hIndex;
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+      minPrice = Math.min(minPrice, prices[i]);
+      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+    return maxProfit;
   }
   static maxProfit2(prices: number[]): number {
     let maxProfit = 0;
@@ -69,8 +68,43 @@ export class ArrayLeetcode150 {
         maxProfit += prices[i] - prices[i - 1];
       }
     }
-
     return maxProfit;
+  }
+
+  static removeElement(nums: number[], val: number): number {
+    let l = 0;
+    for (let r = 0; r < nums.length; r++) {
+      if (nums[r] !== val) {
+        nums[l] = nums[r];
+        l++;
+      }
+    }
+    return l;
+  }
+  static removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+
+    let l = 1;
+    for (let r = 1; r < nums.length; r++) {
+      if (nums[r] !== nums[r - 1]) {
+        nums[l] = nums[r];
+        l++;
+      }
+    }
+    return l;
+  }
+
+  static removeDuplicates2(nums: number[]): number {
+    if (nums.length <= 2) return nums.length;
+
+    let l = 2;
+    for (let r = 2; r < nums.length; r++) {
+      if (nums[r] !== nums[l - 1] || nums[r] !== nums[l - 2]) {
+        nums[l] = nums[r];
+        l++;
+      }
+    }
+    return l;
   }
   static merge(nums1: number[], m: number, nums2: number[], n: number): void {
     let i = m - 1; // Index for nums1
@@ -88,7 +122,6 @@ export class ArrayLeetcode150 {
       }
       k--;
     }
-
     // Copy remaining elements of nums2 if any
     while (j >= 0) {
       nums1[k] = nums2[j];
@@ -96,95 +129,45 @@ export class ArrayLeetcode150 {
       k--;
     }
   }
+  static hIndex(citations: number[]): number {
+    citations.sort((a, b) => a - b);
 
+    let hIndex = 0;
+    let n = citations.length;
 
-  static removeDuplicates(nums: number[]): number {
-    if (nums.length === 0) return 0; // Edge case: empty array
-
-    let k = 1; // Index to keep track of the position for unique elements
-
-    // Iterate through the array starting from the second element
-    for (let i = 1; i < nums.length; i++) {
-      // If the current element is different from the previous element, move it to the position indicated by k
-      if (nums[i] !== nums[i - 1]) {
-        nums[k] = nums[i];
-        k++;
+    for (let i = 0; i < n; i++) {
+      // The h-index is the maximum value of h such that citations[i] >= i + 1
+      if (citations[i] >= n - i) {
+        hIndex = Math.max(hIndex, n - i);
       }
     }
 
-    return k; // Return the number of unique elements
+    return hIndex;
   }
-
-  static removeDuplicates2(nums: number[]): number {
-    if (nums.length <= 2) return nums.length; // Edge case: array length <= 2
-
-    let k = 2; // Index to keep track of the position for the next unique element
-
-    // Iterate through the array starting from the third element
-    for (let i = 2; i < nums.length; i++) {
-      // If the current element is different from the two elements before it, include it in the result array
-      if (nums[i] !== nums[k - 1] || nums[i] !== nums[k - 2]) {
-        nums[k] = nums[i];
-        k++;
-      }
-    }
-
-    return k; // Return the number of unique elements
-  }
-
-  static majorityElementBoyerMoore(a: number[]): number {
-    let majorityElement = a[0],
-      count = 1;
-    for (let i = 1; i < a.length; i++) {
-      count += a[i] === majorityElement ? 1 : -1;
-      if (count === 0) {
-        count = 1;
-        majorityElement = a[i];
-      }
-    }
-    return majorityElement;
-  }
-  static maxProfit(prices: number[]): number {
-    if (prices.length === 0) return 0;
-
-    let minPrice = prices[0];
-    let maxProfit = 0;
-
-    for (let i = 1; i < prices.length; i++) {
-      minPrice = Math.min(minPrice, prices[i]);
-
-      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
-    }
-
-    return maxProfit; // Return the maximum profit
-  }
-
 }
 class RandomizedSet {
   set: Array<number>;
   constructor() {
-      this.set = new Array<number>();
+    this.set = new Array<number>();
   }
-  
+
   insert(val: number): boolean {
-      if (!this.set.includes(val)) {
-          this.set.push(val);
-          return true;
-      }
-      else return false;
+    if (!this.set.includes(val)) {
+      this.set.push(val);
+      return true;
+    } else return false;
   }
 
   remove(val: number): boolean {
-      const index = this.set.indexOf(val);
-      if (index !== -1) {
-          this.set.splice(index, 1);
-          return true;
-      }
-      else return false;
+    const index = this.set.indexOf(val);
+    if (index !== -1) {
+      this.set.splice(index, 1);
+      return true;
+    } else return false;
   }
 
   getRandom(): number {
-      if(this.set.length === 0) return -1;
-      else return this.set[Math.floor(Math.random() * this.set.length)];
+    if (this.set.length === 0) return -1;
+    else return this.set[Math.floor(Math.random() * this.set.length)];
   }
 }
