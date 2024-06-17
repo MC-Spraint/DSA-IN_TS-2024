adba421ee7f9f78f3a7c5922e867bc2197b6c34d
MCSpraint <mcspraint954@gmail.com>
2024-06-17 21:13:55 +0530

    new questions

--- interview_prep/NodeJs/streams.ts
+++ interview_prep/NodeJs/streams.ts
@@ -200,3 +200,30 @@ function promisify(original) {
 
 
 // console.log(fcall1); // Outputs: "att1 - innerProperty"
+
+
+import fs from 'fs';
+
+const readableStream = fs.createReadStream('deploy.sh');
+const writableStream = fs.createWriteStream('output.txt');
+
+// Pipe the readable stream to the writable stream
+readableStream.pipe(writableStream);
+
+// Listen for events on the readable stream
+readableStream.on('data', (chunk) => {
+  console.log('Received chunk:', chunk);
+});
+
+readableStream.on('end', () => {
+  console.log('Readable stream ended');
+});
+
+// Listen for events on the writable stream
+writableStream.on('finish', () => {
+  console.log('Writable stream finished writing');
+});
+
+writableStream.on('error', (err) => {
+  console.error('Error writing to writable stream:', err);
+});
