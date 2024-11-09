01cedf833c2ff40a0cf1269102154566ff8428ae
MCSpraint <mcspraint954@gmail.com>
2024-11-09 08:49:48 +0530

    fix

--- interview_prep/DSA/DSA/string+arrays/arrays/sliding_window/fixed-size-window.ts
+++ interview_prep/DSA/DSA/string+arrays/arrays/sliding_window/fixed-size-window.ts
@@ -79,24 +79,19 @@ export class FixedSizeWindow {
   // replace up to k characters in s to create the longest possible substring where all characters are the same
   //Input: s = "AABABBA", k = 1
   //Output: 4  // Replace one 'B' to get "AAAA" or one 'A' to get "BBBB"
-  static characterReplacement(s: string, k: number): number {
+  public replaceCharUptoKToGetLongestRepeating(s: string, k: number): number {
     const charCount = new Map<string, number>();
-    let maxFreq = 0; // Track the highest frequency of a single character in the current window
-    let maxLength = 0;
 
-    for (let l = 0, r = 0; r < s.length; r++) {
-      // Update the count of the current character
-      const rc = s[r];
-      charCount.set(rc, (charCount.get(rc) || 0) + 1);
-      // Update maxFreq based on the current window
-      maxFreq = Math.max(maxFreq, charCount.get(rc)!);
-      // If current window size - maxFreq > k, it's not valid, shrink window from the l
-      while (r - l + 1 > k + maxFreq) {
-        const lc = s[l];
-        charCount.set(lc, charCount.get(lc)! - 1);
+    let maxFreq = 0;
+    let maxLength = 0;
+    for (let r = 0, l = 0; r < s.length; r++) {
+      charCount.set(s[r], (charCount.get(s[r]) || 0) + 1);
+
+      maxFreq = Math.max(maxFreq, charCount.get(s[r])!);
+      while (r - l + 1 - maxFreq > k) {
+        charCount.set(s[l], charCount.get(s[l])! - 1);
         l++;
       }
-      // Update maxLength with the size of the valid window
       maxLength = Math.max(maxLength, r - l + 1);
     }
     return maxLength;
